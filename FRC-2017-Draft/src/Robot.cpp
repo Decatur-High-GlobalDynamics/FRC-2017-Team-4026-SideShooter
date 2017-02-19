805990f8a41f33dccea5877701f217f56c9991da
jeremybob <jeremybob@gmail.com>
2017-02-19 00:52:10 -0500

    - Merged code from driver station laptop - Added two autonomous modes (untested). Should implement voltage compensation via Talon in the future..encoders would be better. - Fixed shooter tuning (still needs improvement) - Shot with top spin seems to work..needs more testing - Added code for automating the gear catcher that Walden wrote

--- FRC-2017-Draft/src/Robot.cpp
+++ FRC-2017-Draft/src/Robot.cpp
@@ -12,7 +12,9 @@
 #define PI 3.14159265
 #define USE_DRIVE_TIMER 1
 #define DRIVE_TICKSPERREV 1000
-#define SERVO_UP 0.6
+#define SERVO_UP 0.2
+#define SERVO_DOWN 1.0
+#define USE_DRIVE_TIMER 1
 
 /**
  * This is a demo program showing the use of the RobotDrive class.
@@ -45,6 +47,7 @@ class Robot: public frc::SampleRobot {
 	AnalogGyro driveGyro { 0 };
 	AnalogInput wallDistanceSensorR { 2 };
 	AnalogInput wallDistanceSensorL { 1 };
+	DigitalInput photoElectric {2};
 	DigitalInput gearCatcherLimitLeft { 1 };
 	DigitalInput gearCatcherLimitRight { 0 };
 
@@ -57,12 +60,15 @@ class Robot: public frc::SampleRobot {
 	bool agitatorUp;
 	bool genericTimerStarted;
 	int autoState;
+	int gearCatcherState;
+	double avgShooterVelocityError;
 
 	frc::SendableChooser<std::string> chooser;
 	const std::string autoNameDefault = "Default";
-	const std::string autoNameGear1= "Gear Location 1";
-	const std::string autoNameGear2 = "Gear Location 2";
-	const std::string autoNameGear3 = "Gear Location 3";
+	const std::string autoNameGear1RED= "RED: Gear Location 1";
+	const std::string autoNameGear2RED = "RED: Gear Location 2";
+	const std::string autoNameGear3RED = "RED: Gear Location 3";
+	const std::string autoNameTwoHopperRED = "RED: Two Hopper";
 
 public:
 	Robot() {
@@ -70,16 +76,19 @@ public:
 		driveReverse = false;
 		isGyroResetTelop = false;
 		autoState = 0;
+		gearCatcherState = 0;
 		agitatorUp = false;
 		genericTimerStarted = false;
+		avgShooterVelocityError = 0.0;
 		//myRobot.SetExpiration(0.1);
 	}
 
 	void RobotInit() {
 		chooser.AddDefault(autoNameDefault, autoNameDefault);
-		chooser.AddObject(autoNameGear1, autoNameGear1);
-		chooser.AddObject(autoNameGear2, autoNameGear2);
-		chooser.AddObject(autoNameGear3, autoNameGear3);
+		chooser.AddObject(autoNameGear1RED, autoNameGear1RED);
+		chooser.AddObject(autoNameGear2RED, autoNameGear2RED);
+		chooser.AddObject(autoNameGear3RED, autoNameGear3RED);
+		chooser.AddObject(autoNameTwoHopperRED, autoNameTwoHopperRED);
 		frc::SmartDashboard::PutData("Auto Modes", &chooser);
 
 		//Configure Shooter Talons
@@ -90,7 +99,7 @@ public:
 		shooterWheelFront.SetSensorDirection(false);
 		shooterWheelFront.SetAllowableClosedLoopErr(0);
 		shooterWheelFront.SelectProfileSlot(0);
-		shooterWheelFront.SetF(0.0416);
+		shooterWheelFront.SetF(0.02497); //0.0416
 		shooterWheelFront.SetP(0.0);
 		shooterWheelFront.SetI(0.0);
 		shooterWheelFront.SetD(0.0);
@@ -103,7 +112,7 @@ public:
 		shooterWheelBack.SetSensorDirection(false);
 		shooterWheelBack.SetAllowableClosedLoopErr(0);
 		shooterWheelBack.SelectProfileSlot(0);
-		shooterWheelBack.SetF(0.0416);
+		shooterWheelBack.SetF(0.02497);
 		shooterWheelBack.SetP(0.0);
 		shooterWheelBack.SetI(0.0);
 		shooterWheelBack.SetD(0.0);
@@ -123,7 +132,7 @@ public:
 	/*
 	 * Returns true when time in seconds has expired
 	 */
-	bool WaitAsyncUntil(double timeInSec)
+	bool WaitAsyncUntil(double timeInSec, bool resetTimer)
 	{
 		if(!genericTimerStarted)
 		{
@@ -134,7 +143,8 @@ public:
 
 		if(genericTimer.Get() >= timeInSec)
 		{
-			genericTimerStarted = false;
+			if(resetTimer)
+				genericTimerStarted = false;
 			return true;
 		}
 
@@ -146,7 +156,7 @@ public:
 	 */
 	float smoothJoyStick(float joyInput)
 	{
-		return powf(joyInput,3);
+		return powf(joyInput,2);
 	}
 
 	/*
@@ -229,90 +239,96 @@ public:
 	}
 
 	/*
-	 * Simple manual shooter control.  A more complex shooting scheme will be needed to step through opening the door and waiting until the shooter is at speed.
+	 * Spin shooter wheels up to speed with door closed.
+	 * Return true when wheels are at speed
+	 */
+
+	bool spinShooterWheels(double frontWheel, double backWheel)
+	{
+
+		shooterWheelFront.SetP(0.047);
+		shooterWheelFront.SetI(0.0);
+		shooterWheelFront.SetD(1.2);
+
+		shooterWheelBack.SetP(0.047);
+		shooterWheelBack.SetI(0.0);
+		shooterWheelBack.SetD(1.2);
+
+		shooterWheelFront.Set(-1.0 * frontWheel);
+		shooterWheelBack.Set(backWheel);
+
+		avgShooterVelocityError = (shooterWheelFront.GetClosedLoopError() + shooterWheelBack.GetClosedLoopError()) / 2.0;
+
+		if(avgShooterVelocityError < 500)
+			return true;
+
+		return false;
+	}
+
+	/*
+	 * Stop shooter wheels from spinning
+	 */
+	void stopShooter()
+	{
+		shooterServo.Set(SERVO_DOWN);
+
+		shooterWheelFront.SetP(0.0);
+		shooterWheelFront.SetI(0.0);
+		shooterWheelFront.SetD(0.0);
+		shooterWheelBack.SetP(0.0);
+		shooterWheelBack.SetI(0.0);
+		shooterWheelBack.SetD(0.0);
+
+		shooterWheelFront.Set(0.0);
+		shooterWheelBack.Set(0.0);
+	}
+
+	/*
+	 * Perform task of shooting fuel
 	 */
 	void shootFuel()
 	{
+		if(spinShooterWheels(3600.0, 3400.0))
+			shooterServo.Set(SERVO_UP);
+		else
+			shooterServo.Set(SERVO_DOWN);
+
+		if(!agitatorUp && (agitatorTimer.Get() > 0.5))
+		{
+			agitatorServo.Set(0.2);
+			agitatorTimer.Reset();
+			agitatorTimer.Start();
+			agitatorUp = true;
+		}
+		else if(agitatorTimer.Get() > 0.5)
+		{
+			agitatorServo.Set(0.9);
+			agitatorTimer.Reset();
+			agitatorTimer.Start();
+			agitatorUp = false;
+		}
+	}
+
+	/*
+	 * Simple manual shooter control.  A more complex shooting scheme will be needed to step through opening the door and waiting until the shooter is at speed.
+	 */
+	void shootFuelControl()
+	{
 		if(manipulatorStick.GetY() > 0.1 || manipulatorStick.GetY() < -0.1)
 		{
-			shooterWheelFront.SetP(0.047);
-			shooterWheelFront.SetI(0.0);
-			shooterWheelFront.SetD(1.2);
-
-			shooterWheelBack.SetP(0.047);
-			shooterWheelBack.SetI(0.0);
-			shooterWheelBack.SetD(1.2);
-
-			shooterServo.Set(SERVO_UP);
-			shooterWheelFront.Set(-1.0*manipulatorStick.GetY() * 3400);
-			shooterWheelBack.Set(manipulatorStick.GetY()* 3600);
+			if(spinShooterWheels(manipulatorStick.GetY() * 3600.0, manipulatorStick.GetY() * 3400.0))
+				shooterServo.Set(SERVO_UP);
+			else
+				shooterServo.Set(SERVO_DOWN);
 		}
 		else if(manipulatorStick.GetRawButton(1))
 		{
-			//CHANGE VALUES BELOW TO REFLECT ACTUAL SHOOTING SPEED
-			shooterWheelFront.SetP(0.046);
-			shooterWheelFront.SetI(0.0);
-			shooterWheelFront.SetD(1.2);
-
-			shooterWheelBack.SetP(0.046);
-			shooterWheelBack.SetI(0.0);
-			shooterWheelBack.SetD(1.2);
-
-			shooterServo.Set(SERVO_UP);
-			if(!agitatorUp && (agitatorTimer.Get() > 0.5))
-			{
-				agitatorServo.Set(0.2);
-				agitatorTimer.Reset();
-				agitatorTimer.Start();
-				agitatorUp = true;
-			}
-			else if(agitatorTimer.Get() > 0.5)
-			{
-				agitatorServo.Set(0.9);
-				agitatorTimer.Reset();
-				agitatorTimer.Start();
-				agitatorUp = false;
-			}
-
-			shooterWheelFront.Set(-3400.0);
-			shooterWheelBack.Set(3600.0);
+			shootFuel();
 		}
-		/*else if(manipulatorStick.GetRawButton(2))
-		{
-			//CHANGE VALUES BELOW TO REFLECT ACTUAL SHOOTING SPEED
-			shooterServo.Set(SERVO_UP);
-			shooterWheelFront.Set(-0.65);
-			shooterWheelBack.Set(0.65);
-		}
-		else if(manipulatorStick.GetRawButton(3))
-		{
-			//CHANGE VALUES BELOW TO REFLECT ACTUAL SHOOTING SPEEDvv
-			shooterServo.Set(SERVO_UP);
-			shooterWheelFront.Set(-0.7);
-			shooterWheelBack.Set(0.7);
-		}
-		else if(manipulatorStick.GetRawButton(4))
-		{
-			//CHANGE VALUES BELOW TO REFLECT ACTUAL SHOOTING SPEED
-			shooterServo.Set(SERVO_UP);
-			shooterWheelFront.Set(-0.75);
-			shooterWheelBack.Set(0.75);
-		}*/
 		else
 		{
 			//Stop shooting
-			agitatorServo.Set(1.0); //Make sure agitator is down before closing door
-			shooterServo.Set(1.0);
-
-			shooterWheelFront.SetP(0.0);
-			shooterWheelFront.SetI(0.0);
-			shooterWheelFront.SetD(0.0);
-			shooterWheelBack.SetP(0.0);
-			shooterWheelBack.SetI(0.0);
-			shooterWheelBack.SetD(0.0);
-
-			shooterWheelFront.Set(0.0);
-			shooterWheelBack.Set(0.0);
+			stopShooter();
 
 			agitatorUp = false;
 			agitatorTimer.Reset();
@@ -327,17 +343,26 @@ public:
 	{
 		//Should set a dead-zone for this despite the speed controllers having one built in
 		//gearCatcherScrew.Set(manipulatorStick.GetX());
-		if(manipulatorStick.GetRawButton(5) && gearCatcherLimitLeft.Get())
+		if(manipulatorStick.GetRawButton(3))
 		{
-			gearCatcherScrew.Set(0.7);
-		}
-		else if(manipulatorStick.GetRawButton(6))
-		{
-			gearCatcherScrew.Set(-0.7);
+			findGearCatcherLift();
 		}
 		else
 		{
-			gearCatcherScrew.Set(0.0);
+			if(manipulatorStick.GetRawButton(5) && gearCatcherLimitLeft.Get())
+			{
+				gearCatcherScrew.Set(0.7);
+			}
+			else if(manipulatorStick.GetRawButton(6) && gearCatcherLimitRight.Get())
+			{
+				gearCatcherScrew.Set(-0.7);
+			}
+			else
+			{
+				gearCatcherScrew.Set(0.0);
+			}
+
+			gearCatcherState = 0;
 		}
 	}
 
@@ -350,6 +375,16 @@ public:
 		{
 			ballIntakeRoller1.Set(1.0);
 			ballIntakeRoller2.Set(1.0);
+		}
+		else if(manipulatorStick.GetRawButton(8))
+		{
+			ballIntakeRoller1.Set(-1.0);
+			ballIntakeRoller2.Set(-1.0);
+		}
+		else if(manipulatorStick.GetRawButton(4))
+		{
+			ballIntakeRoller1.Set(-0.3);
+			ballIntakeRoller2.Set(-0.3);
 		}
 		else
 		{
@@ -376,12 +411,22 @@ public:
 		{
 			autoDriveTimer.Reset();
 			autoDriveTimer.Start();
+			resetGyroAngle();
 		}
 		else
 		{
 			//leftDriveEncoder.Reset();
 			//rightDriveEncoder.Reset();
+			//resetGyroAngle();
 		}
+	}
+
+	/*
+	 * Reset the Gyro position
+	 */
+	void resetGyroAngle()
+	{
+		driveGyro.Reset();
 	}
 
 	/*
@@ -529,6 +574,77 @@ public:
 
 		SmartDashboard::PutNumber("GearLimitLeft: ", gearCatcherLimitLeft.Get());
 		SmartDashboard::PutNumber("GearLimitRight: ", gearCatcherLimitRight.Get());
+		SmartDashboard::PutNumber("photoElectric: ", photoElectric.Get());
+
+		SmartDashboard::PutNumber("Avg Shooter Vel Error: ", avgShooterVelocityError);
+	}
+
+	/*
+	 * Align robot parallel with wall
+	 */
+	float performRobotFaceAlignment()
+	{
+		float angleToTurnDegrees = 0.0;
+		try
+		{
+			angleToTurnDegrees = (atan((CalculateWallDistanceR(false) - CalculateWallDistanceL(false)) / 22.625) * 180.0) / PI;
+		}
+		catch(...)
+		{
+			angleToTurnDegrees = 0.0;
+		}
+		SmartDashboard::PutNumber("Angle Off Parallel: ", angleToTurnDegrees);
+
+		return angleToTurnDegrees;
+	}
+
+	/*
+	 * Automatically find the retro-reflective tape for the gear lift
+	 */
+	bool findGearCatcherLift()
+	{
+
+		switch(gearCatcherState)
+		{
+			case 0:
+				if (gearCatcherLimitRight.Get()){
+					gearCatcherScrew.Set(-0.7);
+				}
+				else
+				{
+					gearCatcherState++;
+					gearCatcherScrew.Set(0.0);
+				}
+				break;
+			case 1:
+				if (photoElectric.Get() && gearCatcherLimitLeft.Get())
+				{
+					gearCatcherScrew.Set(0.4);
+				}
+				else
+				{
+					gearCatcherState++;
+					gearCatcherScrew.Set(0.0);
+				}
+				break;
+			case 2:
+				if (!photoElectric.Get())
+				{
+					gearCatcherScrew.Set(0.0);
+					return true;
+				}
+				else
+				{
+					gearCatcherState = 0;
+				}
+				break;
+
+			default:
+				gearCatcherScrew.Set(0.0);
+				break;
+		}
+
+		return false;
 	}
 
 	/*
@@ -545,23 +661,248 @@ public:
 	}
 
 	/*
-	 * Align robot parallel with wall
+	 * Get both middle and close hopper of balls (RED)
+	 * I've assumed that positive angles will turn clockwise relative to the gear catcher being the front
+	 * I've assume positive drive motor speeds will drive the robot in reverse (relative to the gear catcher being the front)
 	 */
-	bool performRobotFaceAlignment()
+	void score_RED_TwoHopper_Autonomous()
 	{
-		float angleToTurnDegrees = 0.0;
-		try
+		switch(autoState)
 		{
-			angleToTurnDegrees = (atan((CalculateWallDistanceR(false) - CalculateWallDistanceL(false)) / 22.625) * 180.0) / PI;
-		}
-		catch(...)
-		{
-			angleToTurnDegrees = 0.0;
-		}
-		SmartDashboard::PutNumber("Angle Off Parallel: ", angleToTurnDegrees);
+			case 0:
+				resetDrive(USE_DRIVE_TIMER);
+				autoState++;
+				break;
+			case 1:
+				//Drive the robot in reverse to get to middle hopper
+				if(autoDriveRobot(0.8, 0.8, 2.0, 0, USE_DRIVE_TIMER))
+				{
+					resetDrive(USE_DRIVE_TIMER);
+					autoState++;
+				}
+				break;
+			case 2:
+				//Turn intake side towards hopper
+				if(turnGyro(90.0))
+				{
+					resetDrive(USE_DRIVE_TIMER);
+					autoState++;
+				}
+				break;
+			case 3:
+				//Drive the robot reverse to trigger hopper
+				if(autoDriveRobot(0.5, 0.5, 0.5, 0, USE_DRIVE_TIMER))
+				{
+					resetDrive(USE_DRIVE_TIMER);
+					autoState++;
+				}
+				break;
+			case 4:
+				//Wait some time for the hopper to empty into robot
+				Wait(1.0);
+				resetDrive(USE_DRIVE_TIMER);
+				autoState++;
+				break;
+			case 5:
+				//Drive the robot forward away from hopper
+				if(autoDriveRobot(-0.5, -0.5, 0.5, 0, USE_DRIVE_TIMER))
+				{
+					resetDrive(USE_DRIVE_TIMER);
+					autoState++;
+				}
+				break;
+			case 6:
+				//Turn gear catcher towards alliance station
+				if(turnGyro(-90.0))
+				{
+					resetDrive(USE_DRIVE_TIMER);
+					autoState++;
+				}
+				break;
+			case 7:
+				//Drive the robot in forward to get to close hopper
+				if(autoDriveRobot(0.8, 0.8, 1.0, 0, USE_DRIVE_TIMER))
+				{
+					resetDrive(USE_DRIVE_TIMER);
+					autoState++;
+				}
+				break;
+			case 8:
+				//Turn shooter to face boiler
+				if(turnGyro(60.0))
+				{
+					resetDrive(USE_DRIVE_TIMER);
 
-		return false;
-		//return turnGyro(angleToTurnDegrees);
+					agitatorUp = false;
+					agitatorTimer.Reset();
+					agitatorTimer.Start();
+
+					autoState++;
+				}
+				break;
+			case 9:
+				shootFuel();
+				if(WaitAsyncUntil(4.0, true))
+				{
+					stopShooter();
+					resetDrive(USE_DRIVE_TIMER);
+					autoState++;
+				}
+				break;
+			case 10:
+				//Turn towards close hopper
+				if(turnGyro(30.0))
+				{
+					resetDrive(USE_DRIVE_TIMER);
+					autoState++;
+				}
+				break;
+			case 11:
+				//Drive the robot reverse to trigger hopper
+				if(autoDriveRobot(0.5, 0.5, 0.5, 0, USE_DRIVE_TIMER))
+				{
+					resetDrive(USE_DRIVE_TIMER);
+					autoState++;
+				}
+				break;
+			case 12:
+				//Wait some time for the hopper to empty into robot
+				Wait(1.0);
+				resetDrive(USE_DRIVE_TIMER);
+				autoState++;
+				break;
+			case 13:
+				//Drive the robot forward away from hopper
+				if(autoDriveRobot(-0.5, -0.5, 0.5, 0, USE_DRIVE_TIMER))
+				{
+					resetDrive(USE_DRIVE_TIMER);
+					autoState++;
+				}
+				break;
+			case 14:
+				//Turn shooter to face boiler
+				if(turnGyro(-30.0))
+				{
+					resetDrive(USE_DRIVE_TIMER);
+
+					agitatorUp = false;
+					agitatorTimer.Reset();
+					agitatorTimer.Start();
+
+					autoState++;
+				}
+				break;
+			case 15:
+				shootFuel();
+				if(WaitAsyncUntil(5.0, true))
+				{
+					stopShooter();
+					resetDrive(USE_DRIVE_TIMER);
+					autoState++;
+				}
+				break;
+			default:
+				stopRobotDrive();
+				break;
+		}
+	}
+
+	/*
+	 * Score gear on peg RED (Gear position 1 is closest to the boiler)
+	 * I've assumed that positive angles will turn clockwise relative to the gear catcher being the front
+	 * I've assume positive drive motor speeds will drive the robot in reverse (relative to the gear catcher being the front)
+	 */
+	void score_RED_GearPosition1_Autonomous()
+	{
+		float angleErrorFromUltrasonics = 0.0;
+
+		switch(autoState)
+		{
+			case 0:
+				resetDrive(USE_DRIVE_TIMER);
+				autoState++;
+				break;
+			case 1:
+				//Drive the robot in reverse
+				if(autoDriveRobot(0.8, 0.8, 1.0, 0, USE_DRIVE_TIMER))
+				{
+					resetDrive(USE_DRIVE_TIMER);
+					autoState++;
+				}
+				break;
+			case 2:
+				if(turnGyro(45))
+				{
+					resetDrive(USE_DRIVE_TIMER);
+					autoState++;
+				}
+				break;
+			case 3:
+				//Drive the robot forward to get a bit closer to airship
+				if(autoDriveRobot(-0.5, -0.5, 0.5, 0, USE_DRIVE_TIMER))
+				{
+					resetDrive(USE_DRIVE_TIMER);
+					autoState++;
+				}
+				break;
+			case 4:
+				//Align the robot face parallel to airship wall
+				angleErrorFromUltrasonics = performRobotFaceAlignment();
+				if(angleErrorFromUltrasonics > 20.0)	//Assume that if the angle returned is a large value there has been an error with the sensors
+				{
+					autoState = -1;	//Abort
+				}
+				else
+				{
+					resetDrive(USE_DRIVE_TIMER);
+					autoState++;
+				}
+				break;
+			case 5:
+				if(turnGyro(angleErrorFromUltrasonics))
+				{
+					resetDrive(USE_DRIVE_TIMER);
+					autoState++;
+				}
+				break;
+			case 6:
+				//Search for the peg using the photoelectric sensor
+				if(findGearCatcherLift())
+				{
+					resetDrive(USE_DRIVE_TIMER);
+					autoState++;
+				}
+				else if(WaitAsyncUntil(2.0, true))
+				{
+					autoState = -1; //Abort due to timeout
+				}
+				break;
+			case 7:
+				//Drive the robot forward to get the gear on the peg
+				if(autoDriveRobot(-0.3, -0.3, 1.0, 0, USE_DRIVE_TIMER))
+				{
+					resetDrive(USE_DRIVE_TIMER);
+					autoState++;
+				}
+				break;
+			case 8:
+				//Wait some time for the pilot to remove the gear.  Would be better to have a sensor for this.
+				Wait(3.0);
+				resetDrive(USE_DRIVE_TIMER);
+				autoState++;
+				break;
+			case 9:
+				//Drive the robot reverse
+				if(autoDriveRobot(0.5, 0.5, 1.0, 0, USE_DRIVE_TIMER))
+				{
+					resetDrive(USE_DRIVE_TIMER);
+					autoState++;
+				}
+				break;
+			default:
+				stopRobotDrive();
+				break;
+		}
 	}
 
 	/*
@@ -582,16 +923,20 @@ public:
 
 		while (IsAutonomous() && IsEnabled())
 		{
-			if(autoSelected == autoNameGear1){
-				//lowBarAutonomous();
+			if(autoSelected == autoNameGear1RED){
+				score_RED_GearPosition1_Autonomous();
 			}
-			else if (autoSelected == autoNameGear2)
+			else if (autoSelected == autoNameGear2RED)
 			{
 				//moatRampartAutonomous();
 			}
-			else if (autoSelected == autoNameGear3)
+			else if (autoSelected == autoNameGear3RED)
 			{
 				//lowBarScoreAutonomous();
+			}
+			else if (autoSelected == autoNameTwoHopperRED)
+			{
+				score_RED_TwoHopper_Autonomous();
 			}
 			else
 			{
@@ -611,7 +956,7 @@ public:
 		while (IsOperatorControl() && IsEnabled())
 		{
 			tankDrive();
-			shootFuel();
+			shootFuelControl();
 			controlGearCatcher();
 			controlBallIntake();
 			updateDashboard();
